3f231cb37555299670fcd879402610fd32f15490
Zode <jonzqzode@gmail.com>
2025-09-12 21:06:27 +0300

    Slow down AI so it is easier to demo moving against them.

--- Source/Game/DemoAI.cs
+++ Source/Game/DemoAI.cs
@@ -18,7 +18,7 @@ public class DemoAI : Script, IKinematicCharacter
 
 	private const float JUMP_SPEED = 14.0f; // Jump speed in units per second
 	private const float GRAVITY = 30.0f; // Gravity in units per second squared
-	private const float WALK_SPEED = 512.0f; // Walk speed in units per second
+	private const float WALK_SPEED = 10.0f; // Walk speed in units per second
 	private const float WALK_ACCELERATION = 12.0f; // Walk acceleration in units per second squared
 	private const float DECELERATION_SPEED = 10.0f; // Deceleration speed in units per second
 	private const float FRICTION = 6.0f; // Friction coefficient
